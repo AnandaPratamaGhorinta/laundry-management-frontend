3b9777286cb09dcd27ffbc52796faddd138b7b86
AnandaPratamaGhorinta <ghorinta@gmail.com>
2024-08-07 01:35:41 +0700

    update proxy and mock port

--- mockServer/index.ts
+++ mockServer/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-
+const PORT = '4000'
 app.get('/users', (request: Request, response: Response) => {
   response.json([
     { id: 546, username: 'John' },
@@ -14,6 +14,6 @@ app.delete('/users', (request: Request, response: Response) => {
   response.json({ result: 'success' });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log('Server is running on port 4000');
 });
