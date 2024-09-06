aee92c6491b0cc95bc731a26580799e6d1cb9626
AnandaPratamaGhorinta <ghorinta@gmail.com>
2024-09-06 23:01:11 +0700

    remove unused import

--- src/uiComponent/qrCodeSection/QRCodeSection.tsx
+++ src/uiComponent/qrCodeSection/QRCodeSection.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { Button, Layout, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
 import { createUseStyles } from "react-jss";
