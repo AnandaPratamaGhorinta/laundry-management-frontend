2e54ef1fc38677b4afa21b7131a989445358ede0
adhiGho0504 <adhi21gho@gmail.com>
2026-08-22 21:58:50 +0700

    testtest

--- mockServer/index.ts
+++ mockServer/index.ts
@@ -1,19 +1,18 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = '4000'
-app.get('/users', (request: Request, response: Response) => {
+const PORT = "4000";
+app.get("/users", (request: Request, response: Response) => {
   response.json([
-    { id: 546, username: 'John' },
-    { id: 894, username: 'Mary' },
-    { id: 326, username: 'Jane' }
+    { id: 546, username: "John" },
+    { id: 894, username: "Mary" },
   ]);
 });
 
-app.delete('/users', (request: Request, response: Response) => {
-  response.json({ result: 'success' });
+app.delete("/users", (request: Request, response: Response) => {
+  response.json({ result: "success" });
 });
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 4000');
+  console.log("Server is running on port 4000");
 });
